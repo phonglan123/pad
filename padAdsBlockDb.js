e1882bfc27274bf7a479463d2428b8ec04c0e143
phonglan123 <32542949+phonglan123@users.noreply.github.com>
2021-05-13 14:49:30 +0700

    Update video link

--- padAdsBlockDb.js
+++ padAdsBlockDb.js
@@ -6,7 +6,7 @@ var adsBlockDb = [
         'https://desnetvietnam.blogspot.com/p/ang-ky-nhan-bo-9-ky-nang-mem.html | https://1.bp.blogspot.com/-OU40F7bj4YA/XhcrxCWMSjI/AAAAAAAAKlU/rzNryu9tBYEgV4sY9NGJ5CMDyod_pqGxACLcBGAsYHQ/s1600/b%25E1%25BB%2599-9-k%25E1%25BB%25B9-n%25C4%2583ng-m%25E1%25BB%2581m.png | Đăng ký nhận BỘ 9 KỸ NĂNG MỀM'
     ],
     adsVideoDb = [
-        'https://phonglan123.github.io/pad/ | https://a.uguu.se/voFkxbQL.mp4 | Quảng cáo PADAD - Xem free'
+        'https://phonglan123.github.io/pad/ | https://www.w3schools.com/html/movie.mp4 | Quảng cáo PADAD - Xem free'
     ];
 adsBlockDb.forEach(entry => padAdsDb.push({
     l: entry.split(' | ')[0],
