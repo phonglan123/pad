72b1e9a899fe9071bb51d2544aa94ef9049ed5e8
phonglan123 <32542949+phonglan123@users.noreply.github.com>
2021-05-13 13:59:12 +0700

    Update db

--- padAdsBlockDb.js
+++ padAdsBlockDb.js
@@ -1,13 +1,12 @@
 var adsBlockDb = [
-
         'https://phonglan123.github.io/pad/ | https://i.ytimg.com/vi/ER126fHr-Oc/maxresdefault.jpg | Quảng cáo PADAD - Xem free',
-        'https://github.com/phonglan123/pad/blob/main/README.md#đăng-kí-quảng-cáo | https://static.wixstatic.com/media/f9ad87_a7614660741d40c68f1bc8133a5caa33~mv2.gif | Đăng kí đặt quảng cáo hoàn toàn miễn phí'
-
+        'https://github.com/phonglan123/pad/blob/main/README.md#đăng-kí-quảng-cáo | https://static.wixstatic.com/media/f9ad87_a7614660741d40c68f1bc8133a5caa33~mv2.gif | Đăng kí đặt quảng cáo hoàn toàn miễn phí',
+        'https://desnetvietnam.blogspot.com/2019/11/ebook-bi-quyet-bien-nguoi-la-thanh.html | https://mrtranduong.com/wp-content/uploads/2019/10/EBOOK.jpg | Ebook "Bí Quyết Biến Người Lạ Thành Người Quen"',
+        'https://desnetvietnam.blogspot.com/p/pha-mang-la-gi.html | https://1.bp.blogspot.com/-XRIRHcToKeg/XU7SpOcdy0I/AAAAAAAAJ_U/WH69xd2XMuAUkzP7VHtVSrexpqgGtxyKQCLcBGAs/s1600/ios12-no-internet-connection-icon_2x.png | Phá Mạng - Desnet!!!!"',
+        'https://desnetvietnam.blogspot.com/p/ang-ky-nhan-bo-9-ky-nang-mem.html | https://1.bp.blogspot.com/-OU40F7bj4YA/XhcrxCWMSjI/AAAAAAAAKlU/rzNryu9tBYEgV4sY9NGJ5CMDyod_pqGxACLcBGAsYHQ/s1600/b%25E1%25BB%2599-9-k%25E1%25BB%25B9-n%25C4%2583ng-m%25E1%25BB%2581m.png | Đăng ký nhận BỘ 9 KỸ NĂNG MỀM'
     ],
     adsVideoDb = [
-
         'https://phonglan123.github.io/pad/ | https://a.uguu.se/voFkxbQL.mp4 | Quảng cáo PADAD - Xem free'
-
     ];
 adsBlockDb.forEach(entry => padAdsDb.push({
     l: entry.split(' | ')[0],
