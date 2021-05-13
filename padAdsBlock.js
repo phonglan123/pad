bf2aaab92868cc9e5ea5a4f3dced403e37c246eb
phonglan123 <32542949+phonglan123@users.noreply.github.com>
2021-05-13 16:03:51 +0700

    Upgrade

--- padAdsBlock.js
+++ padAdsBlock.js
@@ -161,7 +161,6 @@ addScript('https://desnetnhaponline.blogspot.com/feeds/posts/summary?orderby=pub
 addScript('https://piecablog.blogspot.com/feeds/posts/summary?orderby=published&max-results=500&alt=json-in-script&callback=desnetRssCb');
 addScript('https://desnetvietnam.blogspot.com/feeds/posts/summary?orderby=published&max-results=500&alt=json-in-script&callback=desnetRssCb');
 window.document.onload = docLoaded();
-setTimeout(docLoaded, 1000);
     
 var adsAlert = 'Được tài trợ',
     adsRegisterLink = 'https://github.com/phonglan123/pad/blob/main/README.md#%C4%91%C4%83ng-k%C3%AD-qu%E1%BA%A3ng-c%C3%A1o',
